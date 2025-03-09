0f52b2884e2c4d795d43afc5dfb41ebc58072679
@maptaul <maptaulislam1@gmail.com>
2025-03-09 16:31:52 +0600

    ward add

--- src/constants/images.tsx
+++ src/constants/images.tsx
@@ -1,14 +1,35 @@
 // import { useState } from "react";
 
 export const images = [
-  { src: "/images/photo1.jpg", text: "মাসব্যাপী ফুল উৎসব" },
-  { src: "/images/photo2.jpg", text: "রেস্টুরেন্ট" },
-  { src: "/images/photo3.jpg", text: "নৌকা ভ্রমণ ও কায়াকিং" },
+  {
+    src: "/images/photo2024-03-09-14-44-51_65ec2183ae0e6.jpg",
+    text: "মাসব্যাপী ফুল উৎসব",
+  },
+  {
+    src: "/images/photo2024-12-19-21-54-55_676441cf02975.jpg",
+    text: "রেস্টুরেন্ট",
+  },
+  {
+    src: "/images/photo2024-02-20-01-10-08_65d3a79054b4b.jpg",
+    text: "নৌকা ভ্রমণ ও কায়াকিং",
+  },
   { src: "/images/photo4.jpg", text: "সানসেট ডিউ পয়েন্ট" },
-  { src: "/images/photo5.jpg", text: "সেলফি কর্নার" },
-  { src: "/images/photo6.jpg", text: "কিডস জোন" },
-  { src: "/images/photo7.jpg", text: "মাছ ধরার ব্যবস্থা" },
-  { src: "/images/photo8.jpg", text: "ফুলের বাগান" },
+  {
+    src: "/images/photo2024-11-26-22-25-34_6745f67e12e93.jpg",
+    text: "সেলফি কর্নার",
+  },
+  {
+    src: "/images/photo2024-02-20-01-11-39_65d3a7ebce1df.jpg",
+    text: "কিডস জোন",
+  },
+  {
+    src: "/images/photo2024-02-20-01-12-37_65d3a825be7bf.jpg",
+    text: "মাছ ধরার ব্যবস্থা",
+  },
+  {
+    src: "/images/photo2024-02-20-01-06-56_65d3a6d031802.jpg",
+    text: "ফুলের বাগান",
+  },
 ];
 
 // export default function ImageGallery() {
